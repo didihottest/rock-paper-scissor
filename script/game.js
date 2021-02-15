f63e5649538fe92307b7360ca3ac0cb07991f464
didihottest <didihottest@gmail.com>
2021-02-15 13:42:02 +0700

    game.js logic perfect

--- script/game.js
+++ script/game.js
@@ -1,35 +1,45 @@
-var computerChoose = Math.floor((Math.random() * 3) + 1);
-console.log(computerChoose);
 
+// refresh button
 document.querySelector(".refresh").addEventListener("click", function(){
     location.reload();
 });
 
-
+// player choose algorithm
 document.querySelector(".rock-p").addEventListener("click", function () {
+    var computerChoose = Math.floor((Math.random() * 3) + 1);
+    console.log("computer :"+ computerChoose);
     playerChoose = 1;
     document.querySelector(".rock-p").classList.add("game-img-clicked");
-    comEffect();
+    document.querySelector(".paper-p").classList.remove("game-img-clicked")
+    document.querySelector(".scissor-p").classList.remove("game-img-clicked");
+    comEffect(computerChoose);
     playGame(playerChoose, computerChoose);
 });
 
 document.querySelector(".paper-p").addEventListener("click", function () {
+    var computerChoose = Math.floor((Math.random() * 3) + 1);
+    console.log("computer :"+ computerChoose);
     playerChoose = 2;
     document.querySelector(".paper-p").classList.add("game-img-clicked");
-    comEffect();
+    document.querySelector(".scissor-p").classList.remove("game-img-clicked");
+    document.querySelector(".rock-p").classList.remove("game-img-clicked");
+    comEffect(computerChoose);
     playGame(playerChoose, computerChoose);
 });
 
 document.querySelector(".scissor-p").addEventListener("click", function () {
+    var computerChoose = Math.floor((Math.random() * 3) + 1);
+    console.log("computer :"+ computerChoose);
     playerChoose = 3;
     document.querySelector(".scissor-p").classList.add("game-img-clicked");
-    comEffect();
+    document.querySelector(".rock-p").classList.remove("game-img-clicked");
+    document.querySelector(".paper-p").classList.remove("game-img-clicked");
+    comEffect(computerChoose);
     playGame(playerChoose, computerChoose);
 });
 
-
+// game logic
 let winLose = document.querySelector(".win-lose");
-
 function playGame(playerChoose, computerChoose) {
     if (playerChoose === computerChoose) {
         winLose.classList.remove("vs")
@@ -62,12 +72,20 @@ function playGame(playerChoose, computerChoose) {
     }
 }
 
-function comEffect() {
+// computer padding
+
+function comEffect(computerChoose) {
     if (computerChoose === 1) {
         document.querySelector(".rock-c").classList.add("game-img-clicked");
+        document.querySelector(".paper-c").classList.remove("game-img-clicked")
+        document.querySelector(".scissor-c").classList.remove("game-img-clicked");
     } else if (computerChoose === 2) {
         document.querySelector(".paper-c").classList.add("game-img-clicked");
+        document.querySelector(".scissor-c").classList.remove("game-img-clicked");
+        document.querySelector(".rock-c").classList.remove("game-img-clicked");
     } else if (computerChoose === 3) {
         document.querySelector(".scissor-c").classList.add("game-img-clicked");
+        document.querySelector(".rock-c").classList.remove("game-img-clicked");
+        document.querySelector(".paper-c").classList.remove("game-img-clicked");
     }
 }
