ba661d461e73e4d5dbe3e2f641f8b2b9d605fe0e
didihottest <didihottest@gmail.com>
2021-02-26 12:26:37 +0700

    add game log

--- script/game.js
+++ script/game.js
@@ -7,31 +7,46 @@ let paperCompClass = document.querySelector(".paper-c");
 let scissorCompClass = document.querySelector(".scissor-c");
 let winLose = document.querySelector(".win-lose");
 
-// refresh button
-document.querySelector(".refresh").addEventListener("click", function () {
-    location.reload();
-});
+// choices option
+let choices = ["rock", "paper", "scissor"];
+
+// scores
+let playerScores = 0;
+let computerScores = 0;
+
+// history
+let playerChooseHistory = [];
+let computerChooseHistory = [];
 
 // player choose action listener
 rockPlayerClass.addEventListener("click", function () {
-    let rockAction = new Action(1);
-    rockAction.playGame();
+    let rockAction = new Action(choices[0]);
+    console.log(`player choose ${choices[0]}`);
+    playerChooseHistory.push(choices[0]);
+    console.log(`player choice history ${playerChooseHistory}`);
     rockAction.playerEffect();
     rockAction.comEffect();
+    rockAction.playGame();
 });
 
 paperPlayerClass.addEventListener("click", function () {
-    let paperAction = new Action(2);
-    paperAction.playGame();
+    let paperAction = new Action(choices[1]);
+    console.log(`player choose ${choices[1]}`);
+    playerChooseHistory.push(choices[1]);
+    console.log(`player choice history ${playerChooseHistory}`);
     paperAction.playerEffect();
     paperAction.comEffect();
+    paperAction.playGame();
 });
 
 scissorPlayerClass.addEventListener("click", function () {
-    let scissorAction = new Action(3);
-    scissorAction.playGame();
+    let scissorAction = new Action(choices[2]);
+    console.log(`player choose ${choices[2]}`);
+    playerChooseHistory.push(choices[2]);
+    console.log(`player choice history ${playerChooseHistory}`);
     scissorAction.playerEffect();
     scissorAction.comEffect();
+    scissorAction.playGame();
 });
 
 // action class when player has choosen
@@ -42,20 +57,24 @@ class Action {
     }
     // computer random choice logic 
     computerChoiceLogic() {
-        return Math.floor((Math.random() * 3) + 1);
+        let computerChoosen = choices[Math.floor((Math.random() * 3))];
+        console.log(`computer choose ${computerChoosen}`);
+        computerChooseHistory.push(computerChoosen);
+        console.log(`computer choice history ${computerChooseHistory}`);
+        return computerChoosen;
     }
 
     // padding effect when player has choosen
     playerEffect(){
-        if (this.playerChoose === 1) {
+        if (this.playerChoose === "rock") {
             rockPlayerClass.classList.add("game-img-clicked");
             paperPlayerClass.classList.remove("game-img-clicked");
             scissorPlayerClass.classList.remove("game-img-clicked");
-        } else if (this.playerChoose === 2) {
+        } else if (this.playerChoose === "paper") {
             paperPlayerClass.classList.add("game-img-clicked");
             scissorPlayerClass.classList.remove("game-img-clicked");
             rockPlayerClass.classList.remove("game-img-clicked");
-        } else if (this.playerChoose === 3) {
+        } else if (this.playerChoose === "scissor") {
             scissorPlayerClass.classList.add("game-img-clicked");
             rockPlayerClass.classList.remove("game-img-clicked");
             paperPlayerClass.classList.remove("game-img-clicked");
@@ -63,15 +82,15 @@ class Action {
     }
     // padding effect when computer has choosen
     comEffect() {
-        if (this.computerChoose === 1) {
+        if (this.computerChoose === "rock") {
             rockCompClass.classList.add("game-img-clicked");
             paperCompClass.classList.remove("game-img-clicked");
             scissorCompClass.classList.remove("game-img-clicked");
-        } else if (this.computerChoose === 2) {
+        } else if (this.computerChoose === "paper") {
             paperCompClass.classList.add("game-img-clicked");
             scissorCompClass.classList.remove("game-img-clicked");
             rockCompClass.classList.remove("game-img-clicked");
-        } else if (this.computerChoose === 3) {
+        } else if (this.computerChoose === "scissor") {
             scissorCompClass.classList.add("game-img-clicked");
             rockCompClass.classList.remove("game-img-clicked");
             paperCompClass.classList.remove("game-img-clicked");
@@ -81,15 +100,22 @@ class Action {
     playGame() {
         if (this.playerChoose === this.computerChoose) {
             this.bothDraw();
-        } else if (this.playerChoose === 1 && this.computerChoose === 2 || 
-            this.playerChoose === 2 && this.computerChoose === 3 || 
-            this.playerChoose === 3 && this.computerChoose === 1) {
+            console.log(`Draw`);
+        } else if (this.playerChoose === "rock" && this.computerChoose === "paper" || 
+            this.playerChoose === "paper" && this.computerChoose === "scissor" || 
+            this.playerChoose === "scissor" && this.computerChoose === "rock") {
             this.computerWin();
-        } else if (this.playerChoose === 1 && this.computerChoose === 3 || 
-            this.playerChoose === 2 && this.computerChoose === 1 || 
-            this.playerChoose === 3 && this.computerChoose === 2) {
+            computerScores ++;
+            console.log(`${this.computerChoose} over ${this.playerChoose} Computer Wins`);
+        } else if (this.playerChoose === "rock" && this.computerChoose === "scissor" || 
+            this.playerChoose === "paper" && this.computerChoose === "rock" || 
+            this.playerChoose === "scissor" && this.computerChoose === "paper") {
             this.playerWin();
+            console.log(`${this.playerChoose} over ${this.computerChoose} Player Wins`);
+            playerScores ++;
         }
+
+        console.log(`Player Scores : ${playerScores} Computer Scores : ${computerScores}`);
     }
 
     // match indicators 
@@ -114,6 +140,10 @@ class Action {
         winLose.innerHTML = "COM WIN";
     }
 
+
 }
 
-
+// refresh button
+document.querySelector(".refresh").addEventListener("click", function () {
+    location.reload();
+});
